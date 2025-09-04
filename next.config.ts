d5dac3392902c26de080d7ade1a3a4ad0e15e63a
riugamine <jangel9829@gmail.com>
2025-09-04 15:49:08 -0400

    Enhance project configuration and styling: Added image domain support for TMDB, updated package dependencies including FontAwesome, React Query, and Zod, and improved global styles with new CSS variables and dark mode support.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,22 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ['image.tmdb.org'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'image.tmdb.org',
+        port: '',
+        pathname: '/t/p/**',
+      },
+    ],
+  },
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '2mb',
+    },
+  },
 };
 
 export default nextConfig;
